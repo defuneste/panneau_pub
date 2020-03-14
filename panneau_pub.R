bb48b407c87eceb05019fefd804a268073b81cac
defuneste <leroy.oli@gmail.com>
2020-03-14 08:46:42 +0100

    prise de notes osm lyon

--- panneau_pub.R
+++ panneau_pub.R
@@ -36,6 +36,13 @@ str(panneau_pub)
 panneau_pub[is.na(panneau_pub$adresse),]
 # cela correspond à l'identifian AB00201 'Pont de la Gravière' à Sainte-Foy-les-lyon
 
+panneau_pub$identifian
+
+panneau_pub$test <-  as.character(substr(panneau_pub$identifian, 1,2))
+
+table(panneau_pub$famillemob)
+
+View(panneau_pub[panneau_pub$test == "MU",])
 # commune et cod_insee pe redondant il y a aussi un NA
 
 panneau_pub[is.na(panneau_pub$commune),]
@@ -54,12 +61,30 @@ ggplot( aes(x = reorder(famillemob, -nombre), y = nombre,  fill = famillemob)) +
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank())
 
+# si ce sont des abri de bus ils doivent être taggé comme aminity = shelter
+# on divisie : Abri et Abri non publicitaire
+# on met de coté abri taxi
+# on fait un jeux pour sucette = MU
+# un grand panneau
+# pour la abri
+# highway = bus stop / public transport = plateform <- a verif par le contributeur  
+# shleter = yes
+# les deux premières lettres de l'identifian correspondent au type de mobilier
+# pour les abri publicitaire advertising = yes 
+# les MU advestising = posterboxe 
+#        size = 0.76 x 1,20          
+# les SV advestising = bilboard 
+#        size = B4 ou c4
+# 
+
 #### 4 Renomage et renforcement des données ============
 
 panneau_pub <- panneau_pub %>%
                   mutate("source:name" = "Mobilier urbain de la Métropole de Lyon",
                          "source:addr" = "https://data.grandlyon.com/jeux-de-donnees/mobilier-urbain-metropole-lyon/ressources")
-  
+
+panneau_pub[panneau_pub$famillemob == "Abri taxi publicitaire",]
+
 #### 5 Export =============
 
 #nb va poser pb avec mon NA sur le code_insee
